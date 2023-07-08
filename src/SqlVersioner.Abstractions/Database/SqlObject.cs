992b4846168ae6258f5c3514c6c200f261f03920
Paul Alves <paulalves@protonmail.com>
2023-07-08 14:35:06 +0800

    make header optional

--- src/SqlVersioner.Abstractions/Database/SqlObject.cs
+++ src/SqlVersioner.Abstractions/Database/SqlObject.cs
@@ -11,17 +11,34 @@ namespace SqlVersioner.Abstractions.Database
   /// </remarks>
   public record SqlObject(string Schema, string ObjectName, string Type, string Definition)
   {
-    public override string ToString()
+    /// <summary>
+    /// Gets the definition of the SQL object.
+    /// </summary>
+    /// <param name="includeHeader">Include header.</param>
+    /// <returns>The definition of the SQL object.</returns>
+    /// <remarks>
+    /// <para>This method is used to get the definition of the SQL object.</para>
+    /// </remarks>
+    public string GetSqlDefinition(bool includeHeader = true)
     {
       var sb = new StringBuilder();
-      sb.AppendLine("/*");
-      sb.AppendLine($"Schema: {Schema}");
-      sb.AppendLine($"Type: {Type}");
-      sb.AppendLine($"ObjectName: {ObjectName}");
-      sb.AppendLine($"Execution: {DateTime.UtcNow.ToString("f")}");
-      sb.AppendLine("*/");
-      sb.AppendLine(Definition);
+      if (includeHeader)
+      {
+        sb.AppendLine("/*");
+        sb.AppendLine($"Schema: {Schema}");
+        sb.AppendLine($"Type: {Type}");
+        sb.AppendLine($"ObjectName: {ObjectName}");
+        sb.AppendLine($"Execution: {DateTime.UtcNow.ToString("f")}");
+        sb.AppendLine("*/");
+      }
+      sb.AppendLine(Definition.ReplaceLineEndings(Environment.NewLine));
       return sb.ToString();
+    }
+    
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+      return GetSqlDefinition(includeHeader: false);
     }
   }
 }
